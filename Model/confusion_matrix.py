fca3ef84c1fb8a54401a1799779e013b116e9404
Giovanni <giovanni.bartolomeo.rg@gmail.com>
2020-07-01 12:54:18 +0200

    Added added matrix scoring system, setup and requirements

--- Model/confusion_matrix.py
+++ Model/confusion_matrix.py
@@ -1,12 +1,19 @@
+from config import CSV_ACTUAL_TN_NUMBER,CSV_ACTUAL_TP_NUMBER
+from texttable import Texttable
+
 class ConfusionMatrix:
     # Confusion matrix generated from expected behaviour csv and actual behaviour csv
     # csv format: test name, category, real vulnerability, cwe, Benchmark version: 1.2, 2016-06-1
 
-    def __init__(self, actual_csv, expected_csv):
+    def __init__(self, actual_csv, expected_csv, tp_number=CSV_ACTUAL_TP_NUMBER, tn_number=CSV_ACTUAL_TN_NUMBER):
         self.TP = 0
         self.TN = 0
         self.FP = 0
         self.FN = 0
+        self.TPR = 0
+        self.FPR = 0
+        self.tp_number = tp_number
+        self.tn_number = tn_number
         self.TP_list = []
         self.TN_list = []
         self.FP_list = []
@@ -44,11 +51,31 @@ class ConfusionMatrix:
                 elif expected == 'false' and actual == 'false':
                     self.TN = self.TN + 1
                     self.TN_list.append(test_name_expected)
+        self.TPR = self.TP/self.tp_number
+        self.FPR = self.FP/self.tn_number
 
     def pretty_print(self):
         # print the confusion matrix on std_out
-        print('Results: \n')
-        print('True Positive: ' + str(self.TP))
-        print('True Negative: ' + str(self.TN))
-        print('False Positive: ' + str(self.FP))
-        print('False Negative: ' + str(self.FN))
+        print('\n Results: \n')
+        t = Texttable()
+        t.add_rows([['', 'Positive', 'Negative'],
+                    ['Total Population', self.tp_number, self.tn_number]])
+        print(t.draw())
+        t = Texttable()
+        t.add_rows([[' ', 'Condition Positive', 'Condition Negative'],
+                    ['Predicted \nCondition \nPositive', self.TP, self.FP],
+                    ['Predicted \nCondition \nNegative', self.FN, self.TN]])
+        print(t.draw())
+
+        sensitivity=self.TPR
+        specificity=1-self.FPR
+        youden_index=sensitivity+specificity-1
+
+        # Final score = Youden Index * 100
+        t = Texttable()
+        t.add_rows([['Final Score: ', youden_index*100],
+                    ['Sensitivity: ', sensitivity],
+                    ['Specificity: ', specificity]])
+        print(t.draw())
+
+
